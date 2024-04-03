fecbb520735402cca6a78b1fefdc164f46013ec5
Thomas N. Jensen <thomasnorupjensen@gmail.com>
2024-04-03 23:04:35 +0200

    Added vite react to project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import styles from './App.module.scss';
+
+function App() {
+
+  return (
+    <div className={styles.topHeader}>
+      <h1>Hello world</h1>
+    </div>
+  )
+}
+
+export default App
