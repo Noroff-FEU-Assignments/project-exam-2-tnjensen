56efaa4913ea17971cf596ee709e2c476dc28ac7
Thomas N. Jensen <thomasnorupjensen@gmail.com>
2024-04-08 13:14:50 +0200

    Edited login and register styling

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -4,25 +4,25 @@ import './login.scss';
 function Login(){
     return(
         <div className="login">
-        <div className="card">
-            <div className="loginLeft">
-                <h1>Sentire</h1>
-                <p>Log in to connect and share with your friends.</p>
-                <span>No account yet ?</span>
-                    <Link to="/register">
-                    <button>Register</button>
-                    </Link>
-            </div>
-            <div className="loginRight">
-                <h1>Login</h1>
-                <form>
-                    <input placeholder="email" aria-label="email" type="email" className="loginEmail" />
-                    <input placeholder="password" aria-label="password" type="password" className="loginPassword" minLength="6" />
-                    <button>Log in</button>       
-                </form>          
+            <div className="card">
+                <div className="login-left">
+                    <h1>Sentire</h1>
+                    <p>Log in to connect and share with your friends.</p>
+                    <span>No account yet ?</span>
+                        <Link to="/register">
+                        <button>Register</button>
+                        </Link>
+                </div>
+                <div className="login-right">
+                    <h1>Login</h1>
+                    <form>
+                        <input placeholder="email" aria-label="email" type="email" className="loginEmail" />
+                        <input placeholder="password" aria-label="password" type="password" className="loginPassword" minLength="6" />
+                        <button>Log in</button>       
+                    </form>          
+                </div>
             </div>
         </div>
-    </div>
     )
 } 
 export default Login;
